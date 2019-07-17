7f2f6dcb138e6fb97bcbd826050409afc9acea6a
Roman Gebrehiwot <rttgg1@gmail.com>
2019-07-17 11:44:51 -0700

    done with lab12

---
+++ lab12/src/Map.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from "react";
+
+import "./App.css";
+
+class Map extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {};
+  }
+
+
+render() {
+  return (
+    <Fragment>
+     <img src= "https://staticmapmaker.com/img/google@2x.png"
+        alt="" />
+       
+    </Fragment>
+  );
+}
+}
+
+  
+
+export default Map;
